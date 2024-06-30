3862f970bbe92065a793f1be5cea77742bfe72c7
zainabkhan87 <158062790+zainabkhan87@users.noreply.github.com>
2024-06-30 18:24:21 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+// let tickets = ["k23", "m567", "s234", "a345", "g456", "e452", "t890", "i3452", "a456","u9076"];
+// let randomindex = Math.floor( Math.random() * tickets.length);
+
+// let winner=tickets[randomindex];
+// alert("winner is" + winner)
+
+
+let tickets=[];
+for(let i=0; i<10; i++);
+{let ticket = prompt('enter your ticket')};
+ticket.push(tickets)
